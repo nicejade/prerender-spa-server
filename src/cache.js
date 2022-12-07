78351878b9ba2a61c30b60c37042650c7bb88f43
杨琼璞 <jeffygisgreat@gmail.com>
2022-12-07 22:17:54 +0800

    :ambulance: avoid malicious intrusion

--- src/cache.js
+++ src/cache.js
@@ -1,8 +1,13 @@
+const Url = require('url')
 const { createClient } = require('redis')
 
-const { redisConf } = require('./config.js')
-
+const { domainNameWhiteList, redisConf } = require('./config.js')
 let client
+
+const isTargetUrl = (path) => {
+	const pathname = Url.parse(path, true).hostname
+	return domainNameWhiteList.includes(pathname)
+}
 
 module.exports = {
 	init: async () => {
@@ -12,8 +17,11 @@ module.exports = {
 	},
 
 	requestReceived: async (req, res, next) => {
+		if (!isTargetUrl(req.prerender.url)) return
+
 		const result = await client.get(req.prerender.url)
 		if (!!result) {
+			console.log(`⚡️ Return Cache: ${req.prerender.url}`)
 			req.prerender.cacheHit = true
 			return res.send(200, result)
 		}
@@ -22,7 +30,10 @@ module.exports = {
 
 	beforeSend: async (req, res, next) => {
 		const prerender = req.prerender
+		if (!isTargetUrl(prerender.url)) return
+
 		if (!prerender.cacheHit && prerender.statusCode == 200) {
+			console.log(`🎉 Cache Hit: ${prerender.url}`)
 			await client.set(prerender.url, prerender.content, {
 				EX: redisConf.expire,
 			})
--- src/config.js
+++ src/config.js
@@ -6,3 +6,6 @@ exports.redisConf = {
 	// redis default expire [seconds] = 10d
 	expire: 60 * 60 * 24 * 10,
 }
+
+// Prevent villains from using functions to invade servers @2022/12/07
+exports.domainNameWhiteList = ['nicelinks.site']
